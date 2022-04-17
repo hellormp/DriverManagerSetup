fd90011130cd8a7a0c72ede54b338f1a5b16b441
“Yaroslav <Yaroslav_Kosovchenko@epam.com>
2022-04-17 23:40:48 +0300

    added CheckTheLink, getTitle mathod logic #xr7tye

--- src/test/java/setupDriver/firstTest.java
+++ src/test/java/setupDriver/firstTest.java
@@ -4,9 +4,28 @@ import org.testng.annotations.Test;
 
 public class firstTest extends startDownTest{
 
-
+    /*1.
+    Check if the user can open the link, after navigation to link > compare the Title(url) with ER
+    *
+    * */
     @Test
-    public void someThings(){
+    public void CheckTheLink(){
         driver.get("https://bonigarcia.dev/webdrivermanager/");
+        String getTitle = driver.getTitle();
+        if (getTitle == driver.getTitle()){
+            System.out.println("ER");
+        };
+
+
+
     }
+    /*2.
+    Check if the Title
+    * */
+    @Test
+    public void getTitle(){
+        System.out.println("lol");
+    }
+
+
 }
