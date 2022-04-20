a5467aed1385b85b6ddfc859b611308de30aea83
“Yaroslav <Yaroslav_Kosovchenko@epam.com>
2022-04-20 23:18:39 +0300

    Add getWait mathod #9gdk2

--- src/test/java/setupDriver/DriverManager.java
+++ src/test/java/setupDriver/DriverManager.java
@@ -1,10 +1,15 @@
 package setupDriver;
 
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DriverManager {
     private static WebDriver driver;
+
     private static  void setDriver(){
         driver = new ChromeDriver(WebDriverConfig.chromeDriverOptions());
         driver.manage().window().maximize();
@@ -15,5 +20,12 @@ public class DriverManager {
             setDriver();
         }
         return driver;
+    }
+    public WebDriverWait getWait(long timeUnits){
+        WebDriverWait webDriverWait = new WebDriverWait(driver,timeUnits);
+        webDriverWait.ignoring(NoSuchElementException.class)
+                .ignoring(ElementNotInteractableException.class)
+                .ignoring(StaleElementReferenceException.class);
+        return webDriverWait;
     }
 }
