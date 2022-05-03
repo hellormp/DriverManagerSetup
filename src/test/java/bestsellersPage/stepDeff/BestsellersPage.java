4d2acdccaaebf4ffd321a2ca77d298d5709c2df6
“Yaroslav <Yaroslav_Kosovchenko@epam.com>
2022-05-04 00:10:06 +0300

    cover new age with tests #4g6ld

--- src/test/java/bestsellersPage/stepDeff/BestsellersPage.java
+++ src/test/java/bestsellersPage/stepDeff/BestsellersPage.java
@@ -8,42 +8,41 @@ import setupDriver.LoginFlow;
 
 public class BestsellersPage extends LoginFlow {
 
-    private String TITLE = "smonedal";
 
-    public String getTITLE() {
-        return driver.getTitle();
-    }
-
-    public void setTITLE(String TITLE) {
-        this.TITLE = TITLE;
-    }
-
-
-    @Test
-    public void BestsellersPage(){
+    @Test(description = "DONE")
+    public void priceRangeSelector(){
         driver.get("https://www.bookdepository.com/");
         driver.findElement(By.xpath("(//a[text()='Bestsellers'])[1]")).click();
         Select priceRange = new Select(driver.findElement(By.xpath("//select[@id='filterPrice']")));
         priceRange.selectByVisibleText("Under €15");
-
-        Assert.assertEquals("Under €15",driver.findElement(By.xpath("//option[@value='low']")));
+        Assert.assertEquals(true,priceRange.isMultiple());
 
     }
-    @Test
-    public void BeastsellersPage(){
+    @Test(description = "DONE")
+    public void availabilitySelector(){
         driver.get("https://www.bookdepository.com/");
         driver.findElement(By.xpath("(//a[text()='Bestsellers'])[1]")).click();
-        Select priceRange = new Select(driver.findElement(By.xpath("//select[@id='filterPrice']")));
-        priceRange.selectByIndex(1);
-        Assert.assertEquals("Under €15",driver.findElement(By.xpath("//option[@value='low']")));
+        Select availability = new Select(driver.findElement(By.xpath("//select[@id='filterAvailability']")));
+        availability.selectByIndex(1); //In stock (68290)
+
+        //Assert.assertEquals("Under €15",driver.findElement(By.xpath("//option[@value='low']")));
     }
-    @Test
-    public void BestsellersdPage(){
+    @Test(description = "DONE")
+    public void languageSelector(){
         driver.get("https://www.bookdepository.com/");
         driver.findElement(By.xpath("(//a[text()='Bestsellers'])[1]")).click();
-        Select priceRange = new Select(driver.findElement(By.xpath("//select[@id='filterPrice']")));
-        priceRange.selectByValue("low");
-        Assert.assertEquals("Under €15",driver.findElement(By.xpath("//option[@value='low']")));
+        Select language = new Select(driver.findElement(By.xpath("//select[@id='filterLang']")));
+        language.selectByValue("454"); //Ukrainian(18)
+
+        //Assert.assertEquals("Under €15",driver.findElement(By.xpath("//option[@value='low']")));
+    }
+    @Test(description = "DONE")
+    public void format(){
+        driver.get("https://www.bookdepository.com/");
+        driver.findElement(By.xpath("(//a[text()='Bestsellers'])[1]")).click();
+        Select format = new Select(driver.findElement(By.xpath("//select[@id='filterFormat']")));
+        format.selectByVisibleText("Audio");//Audio
+        //Assert.assertEquals("Under €15",driver.findElement(By.xpath("//option[@value='low']")));
     }
 
 }
