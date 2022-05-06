d66b02e2b0d339cde160898fc86126bf8b824f5c
“Yaroslav <Yaroslav_Kosovchenko@epam.com>
2022-05-06 22:01:19 +0300

    work with ActionsAction classes #kod4a1

---
+++ src/test/java/bestsellersPage/stepDeff/ActionsActionTests.java
@@ -0,0 +1,45 @@
+package bestsellersPage.stepDeff;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import setupDriver.LoginFlow;
+
+public class ActionsActionTests extends LoginFlow {
+
+    @Test
+    public void writeInSerchField(){
+        driver.get("https://www.bookdepository.com/");
+        WebElement searchField = driver.findElement(By.xpath("//input[@aria-label='Search for books by keyword / title / author / ISBN']"));
+
+        Actions builder = new Actions(driver);
+
+        Action searchSend = builder
+                .moveToElement(searchField)
+                .keyDown(searchField, Keys.SHIFT)
+                .sendKeys(searchField, "hello")
+                .build();
+        searchSend.perform();
+
+        WebElement searshButton = driver.findElement(By.xpath("//button[@class='header-search-btn']"));
+        searshButton.click();
+        System.out.println(driver.getTitle());
+
+    }
+    @Test
+    void changeLanguage(){
+        driver.get("https://www.bookdepository.com/");
+        WebElement changeLanguageButton = driver.findElement(By.xpath("//div[@class='right-section']//div//div[@class='tbd-dropdown mob-nav-shop desktop-only dropdown']"));
+        Actions hover = new Actions(driver);
+
+        Action changeLanguageButtonHover = hover
+                .moveToElement(changeLanguageButton)
+                .build();
+        changeLanguageButtonHover.perform();
+    }
+
+
+}
