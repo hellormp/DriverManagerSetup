030ae693aac6afd925e83d31aa495e260fe3a812
“Yaroslav <Yaroslav_Kosovchenko@epam.com>
2022-05-07 00:39:07 +0300

    work with ActionsAction classes #kod4a1

--- src/test/java/bestsellersPage/stepDeff/ActionsActionTests.java
+++ src/test/java/bestsellersPage/stepDeff/ActionsActionTests.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import setupDriver.LoginFlow;
 
